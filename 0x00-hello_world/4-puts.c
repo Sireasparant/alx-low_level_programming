f60f02c1325c741bfcb99d1f2267ee8e6ccb571d
Sireasparant <ugarhutogbo@gmail.com>
2022-09-08 14:02:07 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include <studio.h>
 /**
  *
- * main -entry point
+ * main -Entry point
  *
  * Return: 0 after printing the function
  */
 int main(void)
-{puts("\"Programming is like building a multilingual puzzzle");
+{
+	char myChar[55];
+	strcpy(myChar, "\"Programming is like building a multilingual puzzzle");
+	puts(myChar);
 	return (0);
 }
