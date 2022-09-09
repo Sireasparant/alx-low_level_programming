eaa2e6d0c309240c06158badfa86744e771d3416
Sireasparant <ugarhutogbo@gmail.com>
2022-09-08 18:53:45 -0700

    eddit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - print the standard error sandwiched around the standard output
+ * Return: returns 1
+ **/
+int main(void)
+{
+	write(2, "and that piece of art is useful\"- Dora Korpar,2015-10-19\",59);
+	return (1);
