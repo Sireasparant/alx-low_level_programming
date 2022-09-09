3d7749a270f584e3cc1d3fb5af3e26d340124848
Sireasparant <ugarhutogbo@gmail.com>
2022-09-08 19:24:43 -0700

    next

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - print the standard error sandwiched around the standard output
  *
@@ -6,5 +7,5 @@
  **/
 int main(void)
 {
-	printf(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
