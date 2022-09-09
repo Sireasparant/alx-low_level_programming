bb772404343221516c490168cc2d4b1a5d8e8399
Sireasparant <ugarhutogbo@gmail.com>
2022-09-08 17:40:25 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <studio.h>
+
 /**
  * main - print the string in the put function
  *
  * Description: using the main function
- * this program prints "Programming is like building a multilinqual puzzle"
+ * this program prints "Programming is like building a multilinqual puzzle
  * Return: 0 
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
