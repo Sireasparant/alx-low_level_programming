8370d014a79a7b73a3b0cf0b5ab2e84c07200e39
Sireasparant <ugarhutogbo@gmail.com>
2022-09-10 09:10:51 -0700

    commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,21 +8,21 @@
 int main(void)
 {
 	int p, q;
+
 	for (p = 0; p <= 98; p++)
 	{
-		for (q = p + q; q <= pp; q++)
+		for (q = p + 1; q <= 99; q++)
 		{
-			putchar((p / 10) + '0');
-			putchar((p % 10) + '0');
+			putchar(p / 10 + '0');
+			putchar(p % 10 + '0');
 			putchar(' ');
-		putchar((q / 10) + '0');
-	putchar((q % 10) + '0');
-	
-	if (p != 98 || q != 99)
-	{
-		putchar(',');
-		putchar(' ');
-	}
+			putchar((q / 10) + '0');
+			putchar((q % 10) + '0');
+			if (p != 98 || q != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
