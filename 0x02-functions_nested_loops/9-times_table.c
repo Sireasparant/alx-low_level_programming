e74227e42a42fad3830b8cb1f048103f3d58a6d2
Sireasparant <ugarhutogbo@gmail.com>
2022-09-14 10:30:16 -0700

    commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,24 +4,31 @@
  */
 void times_table(void)
 {
-	int num, mult, prod;
+	int num;
+	int mult;
+	int prod = num * mult;
 
 	for (num = 0; num <= 9; num++)
 	{
-		_putchar('0');
-		
 		for (mult = 1; mult <= 9; mult++)
-		{
-			_putchar(',');
-			_putchar(' ');
-
-			prod = num * mult;
-			if (prod <= 9)
+			prod = num * mult
+			if (j == 0)
+			{
+				_putchar('0')
+			}
+			else if (prod <= 9)
+			{
+				_putchar(',');
 				_putchar(' ');
+				_putchar(prod + '0')
+			}
 			else
+			{
+				_putchar(',');
+				_putchar(' ');
 				_putchar((prod / 10) + '0');
-			_putchar((prod % 10) + '0');
-		}
-		_putchar('\n');
+				_putchar((prod % 10) + '0');
+			}
 	}
+	_putchar('\n');
 }
