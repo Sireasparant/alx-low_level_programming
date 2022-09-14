d909bee13ffb8062bd66b93114701191213bc5f1
Sireasparant <ugarhutogbo@gmail.com>
2022-09-14 10:55:54 -0700

    move

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,25 +10,25 @@ void times_table(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-		for (mult = 1; mult <= 9; mult++)
+		for (mult = 0; mult <=9; mult++)
 			prod = num * mult;
-			if (mult == 0)
-			{
-				_putchar('0')
-			}
-			else if (prod <= 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(prod + '0')
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((prod / 10) + '0');
-				_putchar((prod % 10) + '0');
-			}
+		if (mult == 0)
+		{
+			_putchar('0');
+		}
+		else if (prod <= 9)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(prod + '0');
+		}
+		else
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar((prod / 10) + '0');
+			_putchar((prod % 10) + '0');
+		}
 	}
 	_putchar('\n');
 }
