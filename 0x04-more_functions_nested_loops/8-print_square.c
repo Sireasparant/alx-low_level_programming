9b7e9b600ece316df80f398fac473af548e0ef93
Sireasparant <ugarhutogbo@gmail.com>
2022-09-15 18:01:27 -0700

    m

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,12 +10,12 @@
 void print_square(int size)
 {
 	int inc1, inc2;
-
+	
 	if (size > 0)
 	{
 		for (inc1 = 0; inc1 < size; inc1++)
 		{
-			for (inc2 = 0; inc2 < size; inc2++)
+			for (inc2 = 0; inc2 < (size - 1); inc2++)
 			{
 				putchar('#');
 			}
@@ -25,6 +25,5 @@ void print_square(int size)
 	}
 	else
 	{
-		putchar('\n');
-	}
+		putchar('\n');								}
 }
