b6b04dd8b1b2bd9614ca761e90861f133312bced
Sireasparant <ugarhutogbo@gmail.com>
2022-09-21 23:10:27 +0100

    commited

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,4 +2,23 @@
 #include "stdio.h"
 
 /**
- * 
+ * _strcat - function to concatenate
+ * @dest: param1
+ * @src: param2
+ * Return: string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+    int len = 0, i;
+
+    while (dest[len])
+        len++;
+    for (i = 0; src[i] != 0; i++)
+    {
+        dest[len] = src[i];
+        len += 1;
+    }
+    dest[len] = '\0';
+    return (dest);
+}
