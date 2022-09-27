c04aab332e902cdb234c4db25dd9ada8235bddf1
Sireasparant <ugarhutogbo@gmail.com>
2022-09-26 17:37:16 -0700

    mul commit

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * strstr - a function to locate substring
+ * @haystack: sting in which to check for needle
+ * @needle: substring to find in haystack
+ *
+ * Return: pointer to beginning of needle in haystack or null if no match
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i = 0;
+	unsigned int j = 0;
+
+	while (haystack[i])
+	{
+		while (needle[j] && (haystack[i] == needle[0]))
+		{
+			if (haystack[i + j] == needle[j])
+				j++;
+			else
+				break;
+		}
+		if (needle[j])
+		{
+			i++;
+			j = 0;
+		}
+		else
+			return (haystack + i);
+	}
+	return (0);
+}
