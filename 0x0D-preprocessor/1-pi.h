7ceb1b276a131d2066ce82a02d421d60156684a9
Sireasparant <ugarhutogbo@gmail.com>
2022-10-10 09:47:20 -0700

    learning to create a header file

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -4,17 +4,4 @@
 #include <stdio.h>
 #define PI 3.1415926535
 
-/**
- * PI - using macro to define pi
- *
- * Return: Void
- */
-int main(void)
-{
-	float area, r;
-
-	area = PI * r * r;
-	printf("%.3f\n:", area);
-	return (0);
-}
 #endif
